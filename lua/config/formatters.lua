c57f0190850eefafe252efb81af7ef8f4bbc4713
Alexander Alegre <alexander.alegre@outlook.com>
2025-02-05 12:32:36 -0700

    add black as python formatter

--- lua/config/formatters.lua
+++ lua/config/formatters.lua
@@ -10,13 +10,25 @@ linters.setup {
   },
 }
 
--- Setup Prettier as a formatter (fixes code on save)
+-- Formatters
 local formatters = require "lvim.lsp.null-ls.formatters"
+-- Setup Prettier as a formatter (js/ts)
 formatters.setup {
   {
     command = "prettierd",
     filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "css", "scss", "html", "json", "yaml", "markdown" },
     -- Optional: specify any arguments if needed
     -- args = { "--config", "path/to/your/.prettierrc" },
+  },
+}
+
+-- Setup Black as a formatter (python)
+formatters.setup {
+  -- Other formatters...
+  {
+    command = "black",
+    filetypes = { "python" },
+    -- Black reads from stdin with the "-" argument.
+    args = { "--quiet", "-" },
   },
 }
