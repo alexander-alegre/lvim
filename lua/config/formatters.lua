c290cd9e8d59818b141c7aa15474c4a8d6e8b272
Alexander Alegre <alexander.alegre@outlook.com>
2025-02-07 14:27:01 -0700

    use eslint to format

--- lua/config/formatters.lua
+++ lua/config/formatters.lua
@@ -1,33 +1,28 @@
 -- Enable format on save
 lvim.format_on_save = true
 
--- Setup ESLint as a linter
--- Setup ESLint as a linter
-local linters = require "lvim.lsp.null-ls.linters"
+-- Setup ESLint as a linter and formatter
+local null_ls = require "lvim.lsp.null-ls"
+local linters = null_ls.builtins.diagnostics
+local formatters = null_ls.builtins.formatting
 
 -- Check for .eslintrc.json file at the root of the project
 local eslint_config_exists = vim.fn.filereadable(".eslintrc.json") == 1
 
 if eslint_config_exists then
-  linters.setup {
-    {
-      command = "eslint_d",
-      filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  -- Linting setup
+  null_ls.setup {
+    sources = {
+      linters.eslint_d.with {
+        filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+      },
+      -- Formatting setup
+      formatters.eslint_d.with {
+        filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+      },
     },
   }
 end
-
--- Formatters
-local formatters = require "lvim.lsp.null-ls.formatters"
--- Setup Prettier as a formatter (js/ts)
-formatters.setup {
-  {
-    command = "prettierd",
-    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "css", "scss", "html", "json", "yaml", "markdown" },
-    -- Optional: specify any arguments if needed
-    -- args = { "--config", "path/to/your/.prettierrc" },
-  },
-}
 
 -- Setup Black as a formatter (python)
 formatters.setup {
